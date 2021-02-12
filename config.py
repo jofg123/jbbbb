d62fb6b0aa149d6763ff7fa4982b2aa39b0690f4
jofg123 <78727674+jofg123@users.noreply.github.com>
2021-02-12 09:07:03 +0000

    Create config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+# Token of your telegram bot
+# (https://core.telegram.org/bots#6-botfather)
+TELEGRAM_TOKEN = '1643289724:AAEbOrWnayBRJnKnxHtKU8QmCclDEX0GDaM'
+
+# Credtals for logging to vk
+VK_LOGIN = 'bot@bot.ry'
+VK_PASS = 'bot_password'
+
+# List of admins and moders
+# Example: [ '66303244', 'vk1' ]
+ADMINS_IDS = [  ]
+MODERS_IDS = ADMINS_IDS + [  ]
+
+DATABASE_PATH = 'db.json'
+USERS_PATH = 'users'
